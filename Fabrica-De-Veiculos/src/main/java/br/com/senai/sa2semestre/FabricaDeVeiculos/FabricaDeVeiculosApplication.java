6d2f7e7554aaea8235d78a0aabfe130471e8dca4
felipe_pereira14 <persastons@gmail.com>
2024-05-20 19:53:00 -0300

    Criação do Projeto da SA.

---
+++ Fabrica-De-Veiculos/src/main/java/br/com/senai/sa2semestre/FabricaDeVeiculos/FabricaDeVeiculosApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.sa2semestre.FabricaDeVeiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaDeVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaDeVeiculosApplication.class, args);
+	}
+
+}
