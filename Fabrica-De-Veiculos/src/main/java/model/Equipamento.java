036d1dd2479e563d07062065a6a7d1580dd81b77
felipe_pereira14 <persastons@gmail.com>
2024-05-20 21:37:53 -0300

    Criando a Classe Equipamento.java

---
+++ Fabrica-De-Veiculos/src/main/java/model/Equipamento.java
@@ -0,0 +1,89 @@
+package model;
+
+import jakarta.persistence.Entity;
+
+import java.util.Objects;
+
+@Entity
+public class Equipamento {
+
+    private Long idEquipamento;
+    private String tipoEquipamento;
+    private String descricaoEquipamento;
+    private String estadoEquipamento;
+
+
+    public Equipamento(Long idEquipamento, String tipoEquipamento, String descricaoEquipamento, String estadoEquipamento) {
+        this.idEquipamento = idEquipamento;
+        this.tipoEquipamento = tipoEquipamento;
+        this.descricaoEquipamento = descricaoEquipamento;
+        this.estadoEquipamento = estadoEquipamento;
+    }
+
+
+
+    public Long getIdEquipamento() {
+        return idEquipamento;
+    }
+
+    public void setIdEquipamento(Long idEquipamento) {
+        this.idEquipamento = idEquipamento;
+    }
+
+    public String getTipoEquipamento() {
+        return tipoEquipamento;
+    }
+
+    public void setTipoEquipamento(String tipoEquipamento) {
+        this.tipoEquipamento = tipoEquipamento;
+    }
+
+    public String getDescricaoEquipamento() {
+        return descricaoEquipamento;
+    }
+
+    public void setDescricaoEquipamento(String descricaoEquipamento) {
+        this.descricaoEquipamento = descricaoEquipamento;
+    }
+
+    public String getEstadoEquipamento() {
+        return estadoEquipamento;
+    }
+
+    public void setEstadoEquipamento(String estadoEquipamento) {
+        this.estadoEquipamento = estadoEquipamento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Equipamento that = (Equipamento) o;
+
+        if (!idEquipamento.equals(that.idEquipamento)) return false;
+        if (!Objects.equals(tipoEquipamento, that.tipoEquipamento))
+            return false;
+        if (!Objects.equals(descricaoEquipamento, that.descricaoEquipamento))
+            return false;
+        return Objects.equals(estadoEquipamento, that.estadoEquipamento);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idEquipamento.hashCode();
+        result = 31 * result + (tipoEquipamento != null ? tipoEquipamento.hashCode() : 0);
+        result = 31 * result + (descricaoEquipamento != null ? descricaoEquipamento.hashCode() : 0);
+        result = 31 * result + (estadoEquipamento != null ? estadoEquipamento.hashCode() : 0);
+        return result;
+    }
+    @Override
+    public String toString() {
+        return "Equipamento{" +
+                "idEquipamento=" + idEquipamento +
+                ", tipoEquipamento='" + tipoEquipamento + '\'' +
+                ", descricaoEquipamento='" + descricaoEquipamento + '\'' +
+                ", estadoEquipamento='" + estadoEquipamento + '\'' +
+                '}';
+    }
+}
