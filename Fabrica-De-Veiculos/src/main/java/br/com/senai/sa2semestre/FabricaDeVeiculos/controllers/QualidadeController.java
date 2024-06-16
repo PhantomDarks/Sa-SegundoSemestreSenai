1cad202d46be9af5ae17ed0b6e7bad25845d2cf5
Felipe <104037905+PhantomDarks@users.noreply.github.com>
2024-06-16 10:38:00 -0300

    Atualização das operações CRUD na classe QualidadeController e ajustes na entidade Qualidade.
    - No QualidadeController:
      - Atualizadas as operações REST para refletir as mudanças nas anotações da classe Qualidade, garantindo consistência nas requisições HTTP para criação, atualização, busca e remoção de qualidades.
    
    Essas alterações foram feitas para melhorar a integridade e a eficiência das operações relacionadas às qualidades no sistema da fábrica de veículos.

--- Fabrica-De-Veiculos/src/main/java/br/com/senai/sa2semestre/FabricaDeVeiculos/controllers/QualidadeController.java
+++ Fabrica-De-Veiculos/src/main/java/br/com/senai/sa2semestre/FabricaDeVeiculos/controllers/QualidadeController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/qualidade")
+@RequestMapping("/qualidades")
 public class QualidadeController {
 
     @Autowired
